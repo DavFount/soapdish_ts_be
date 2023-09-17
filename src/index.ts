f916065644813b3e7c55a0cec4272ed5259f659f
Davidalen Fountain <davfount@gmail.com>
2023-09-17 00:25:05 -0700

    Started Implemenation of tests

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config({ path: __dirname + "/../.env" });
+import app from "./app";
 
-import express, { Application } from "express";
-import { routes } from "./routes/index";
-
-const app: Application = express();
 const port = process.env.PORT || 8000;
-
-app.use("/", routes);
-
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
