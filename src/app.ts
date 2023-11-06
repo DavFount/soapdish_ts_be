cbc5ad5204a29d41a711e1618ea2ae0f90053f27
Davidalen Fountain <davfount@gmail.com>
2023-11-06 01:23:36 -0800

    Temporarily allow all origins

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express, { Application } from "express";
 import { v1routes } from "./routes/v1/index";
+import cors from "cors";
 
 const app: Application = express();
 
 app.use("/v1/", v1routes);
+
+const options: cors.CorsOptions = {
+  origin: "*",
+};
+app.use(cors());
 
 export default app;
