b2427808eea04261462a2748d67080c123092138
Davidalen Fountain <davfount@gmail.com>
2023-11-06 20:48:10 -0800

    Updated Cors for Testing

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import cors from "cors";
 const app: Application = express();
 
 app.use("/v1/", v1routes);
-const allowedOrigins = ["http://thesoapdish.test:5173", "https://thesoapdish.app"];
+const allowedOrigins = ["*"];
 const options: cors.CorsOptions = {
   origin: allowedOrigins,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
